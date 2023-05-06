8874235c601fb9ece461f31b521d0b8c5ca4f3cf
neunem <neunem@proton.me>
2023-05-07 02:58:08 +0900

    update sample main file.

--- test.py
+++ test.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(__name__) # Flask app 생성
 
-@app.route('/')
+#이곳에서 제대로 설명되어 있음 https://dschloe.github.io/python/kakao_chatbot/chatbot_calculator/
+
+@app.route('/') # main page route
+def test2(): # main page function
+    return "test2" # main page return
+
+@app.route('/api/test', methods=['POST']) # api/test page route
 def test():
-    return "test2"
+    body = request.get_json() # request body
+    print(body)
+    print(body['userRequest']['utterance'])
 
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "test message"
+                    }
+                }
+            ]
+        }
+    }
+    return responseBody # return response body
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True) # run flask
